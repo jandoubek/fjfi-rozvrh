6a5ad80ed652e8de23025afe685e32b89b273992
VHonzik <honzik.vaclav@gmail.com>
2013-11-05 00:16:14 +0100

    Added XML Import/Export Controllers.

---
+++ Rozvrh/Rozvrh/Controllers/DownloadAsXMLController.cs
@@ -0,0 +1,35 @@
+﻿using Rozrvh;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace MvcApplication1.Controllers
+{
+    public class DownloadAsXMLController : Controller
+    {
+        //
+        // GET: /DownloadAsXML/
+
+        public String Index()
+        {
+            return "To download as XML go to Download/";
+        }
+
+        public ActionResult Download()
+        {
+            var hodiny = new List<ExportHodina>();
+            var ser = new XmlSerializer(typeof(List<ExportHodina>));
+            using (var ms = new MemoryStream())
+            {
+                ser.Serialize(ms, hodiny);
+                var bytes = ms.ToArray();
+                return File(bytes, "text/xml", "FJFIrozvrh.xml");               
+            }
+        }
+
+    }
+}
---
+++ Rozvrh/Rozvrh/Controllers/ImportAsXMLController.cs
@@ -0,0 +1,50 @@
+﻿using Rozrvh;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace MvcApplication1.Controllers
+{
+    public class ImportAsXMLController : Controller
+    {
+        //
+        // GET: /ImportAsXML/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /ImportAsXML/Import
+
+        [HttpPost]
+        public String Import(HttpPostedFileBase file)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<ExportHodina>));
+            List<ExportHodina> hodiny = new List<ExportHodina>();
+            try
+            {
+                StreamReader f = new StreamReader(file.InputStream);
+                hodiny = (List<ExportHodina>)ser.Deserialize(f);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                return "Failed to load XML, the file is corrupted.";
+            }
+            string s = "";
+
+            foreach (ExportHodina h in hodiny)
+            {
+                s += h.Name + " " + h.Day.ToString() + System.Environment.NewLine;
+            }
+
+            return s;
+        }
+
+    }
+}
