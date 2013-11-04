6380bdabf2dd54c4a1aedc9be5f273b56d1ab8ec
VHonzik <honzik.vaclav@gmail.com>
2013-11-05 00:15:03 +0100

    Adding unit testing project. ICalGenerator test added.

---
+++ Rozvrh/Rozvrh.Tests/ICalGeneratorTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rozrvh;
+using System.Collections.Generic;
+using MvcApplication1.Controllers;
+
+namespace Rozvrh.Tests
+{
+    [TestClass]
+    public class ICalGeneratorTest
+    {
+        [TestMethod]
+        public void TestGenerate()
+        {
+
+            var mockA = new Moq.Mock<IExportHodina>();
+            var mockB = new Moq.Mock<IExportHodina>();
+
+            mockA.Setup(m => m.Day).Returns(DayOfWeek.Monday);
+            mockA.Setup(m => m.Lecturer).Returns("OsobaA");
+            mockA.Setup(m => m.Length).Returns(new TimeSpan(2, 0, 0));
+            mockA.Setup(m => m.Name).Returns("PredmetA");
+            mockA.Setup(m => m.Room).Returns("MistnostA");
+            mockA.Setup(m => m.StartTime).Returns(new DateTime(1, 1, 1, 7, 30, 0));
+
+            mockB.Setup(m => m.Day).Returns(DayOfWeek.Tuesday);
+            mockB.Setup(m => m.Lecturer).Returns("OsobaB");
+            mockB.Setup(m => m.Length).Returns(new TimeSpan(1, 0, 0));
+            mockB.Setup(m => m.Name).Returns("PredmetB");
+            mockB.Setup(m => m.Room).Returns("MistnostB");
+            mockB.Setup(m => m.StartTime).Returns(new DateTime(1, 1, 1, 17, 0, 0));
+
+            var hodiny = new List<IExportHodina>();
+            hodiny.Add(mockA.Object);
+            hodiny.Add(mockB.Object);
+
+            DateTime semesterStart = new DateTime(2013, 9, 19);
+            DateTime semesterEnd = new DateTime(2014, 2, 19);
+
+            ICalGenerator gen = new ICalGenerator();
+
+            string actualOutput = gen.generateICal(hodiny,semesterStart,semesterEnd);
+            string stamp = gen.dateTimeDateToICalString(DateTime.Today) +
+                gen.hourToICalString(DateTime.Now.Hour, DateTime.Now.Minute);
+            string semStartA = gen.dateTimeDateToICalString(gen.closestDayFromDateTime(semesterStart,mockA.Object.Day));
+            string semStartB = gen.dateTimeDateToICalString(gen.closestDayFromDateTime(semesterStart,mockB.Object.Day));
+            string semEnd = gen.dateTimeDateToICalString(semesterEnd);
+
+            string goodOutput = @"BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+DTSTAMP:" + stamp + @"
+DTSTART:" + semStartA + @"T073000
+DTEND:" + semStartA + @"T093000
+RRULE:FREQ=WEEKLY;UNTIL=" + semEnd + @"T000000Z
+SUMMARY:PredmetA
+LOCATION:MistnostA
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:" + stamp + @"
+DTSTART:" + semStartB + @"T170000
+DTEND:" + semStartB + @"T180000
+RRULE:FREQ=WEEKLY;UNTIL=" + semEnd + @"T000000Z
+SUMMARY:PredmetB
+LOCATION:MistnostB
+END:VEVENT
+END:VCALENDAR";
+            Assert.AreEqual<string>(goodOutput, actualOutput);
+
+        }
+
+        [TestMethod]
+        public void TestEmpty()
+        {
+            var hodiny = new List<IExportHodina>();
+            DateTime semesterStart = new DateTime();
+            DateTime semesterEnd = new DateTime();
+            ICalGenerator gen = new ICalGenerator();
+            string actualOutput = gen.generateICal(hodiny, semesterStart, semesterEnd);
+
+            string goodOutput = @"BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+END:VCALENDAR";
+            Assert.AreEqual<string>(goodOutput, actualOutput);
+        }
+    }
+}
---
+++ Rozvrh/Rozvrh.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rozvrh.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rozvrh.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93ff67d9-5473-4b15-829f-2379abb1b7ca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
