04f9b680674634e25c6f264244ce099a334ea5d1
smolatom <tomas.smola@centrum.cz>
2013-11-05 20:10:10 +0100

    Changes with respect to naming conventions + removed unused usings

--- Rozvrh/Rozvrh/Models/IModel.cs
+++ Rozvrh/Rozvrh/Models/IModel.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MvcApplication1.Models
 {
     interface IModel
     {
-        // Getters to get filters values
-        List<String> departments { get; }
-        List<String> courses { get; }
-        List<String> groups { get; }
-        List<String> years { get; }
-        List<String> lecturers { get; }
-        List<String> classrooms { get; }
-        List<DayOfWeek> days { get; }
-        List<TimeSpan> times { get; }
+        List<String> Departments { get; }
+        List<String> Courses { get; }
+        List<String> Groups { get; }
+        List<String> Years { get; }
+        List<String> Lecturers { get; }
+        List<String> Classrooms { get; }
+        List<DayOfWeek> Days { get; }
+        List<TimeSpan> Times { get; }
 
-        List<String> getParts(String group);
+        List<String> GetParts(String group);
 
         // most important core function to get the timetable to render
-        List<TimetableField> getTimeTable(List<String> filterValues);
+        List<TimetableField> GetTimeTable(List<String> filterValues);
 
     }   
 }
