a91eca4358b721ff7f437ec643b1bfa5734e3e25
salacric <salacric@fjfi.cvut.cz>
2013-11-20 18:14:16 +0100

    Acronym added to Time.

--- Rozvrh/Rozvrh/Models/Timetable/Time.cs
+++ Rozvrh/Rozvrh/Models/Timetable/Time.cs
@@ -11,12 +11,14 @@ namespace Rozvrh.Models.Timetable
         public int hours { get; private set; }
         public int minutes { get; private set; }
         public int timesOrder { get; private set; }
+        public String acronym { get; private set; }
         public Time(int id, int hours, int minutes, int timesOrder)
         {
             this.id = id;
             this.hours = hours;
             this.minutes = minutes;
             this.timesOrder = timesOrder;
+            this.acronym = hours.ToString() + ":" + minutes.ToString();
         }
     }//= time v xml
 }
