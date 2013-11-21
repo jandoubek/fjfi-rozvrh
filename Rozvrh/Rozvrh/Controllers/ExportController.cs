0261ffb4e633a49142bc465d9cf81e8d9e33fc6b
VHonzik <honzik.vaclav@gmail.com>
2013-11-21 19:58:47 +0100

    ExportController requires fixing once model is singleton. Some problems with CLRF versus LF, to be sure commiting.

--- Rozvrh/Rozvrh/Controllers/ExportController.cs
+++ Rozvrh/Rozvrh/Controllers/ExportController.cs
@@ -11,24 +11,20 @@ namespace Rozvrh.Controllers
         public ActionResult ToICal()
         {
             var ie = new ImportExport();
-            return ie.DownloadAsICAL(generateTestExport(), new DateTime(2013, 9, 23), new DateTime(2013, 12, 22));
+            //Vasek: FIX ME 
+            //Misto bla bla prijde odkaz na singleton model, konkretne na jeho TimetableFields
+            //return ie.DownloadAsICAL(blabla, new DateTime(2013, 9, 23), new DateTime(2013, 12, 22));
+            return null;
 
         }
 
         public ActionResult ToSvg()
         {
             var ie = new ImportExport();
-            return ie.DownloadAsSVG(generateTestExport());
-        }
-
-        private List<IExportHodina> generateTestExport()
-        {
-            var testExport = new List<IExportHodina>();
-            testExport.Add(new ExportHodina("VSBP", DayOfWeek.Monday, new DateTime(2000, 1, 1, 14, 30, 0), new TimeSpan(2, 0, 0), "Vopálka", "B-314"));
-            testExport.Add(new ExportHodina("TPC", DayOfWeek.Tuesday, new DateTime(2000, 1, 1, 8, 30, 0), new TimeSpan(2, 0, 0), "Štamberg", "--"));
-            testExport.Add(new ExportHodina("RAO", DayOfWeek.Tuesday, new DateTime(2000, 1, 1, 13, 30, 0), new TimeSpan(4, 0, 0), "Vrba", "B-115"));
-            testExport.Add(new ExportHodina("CHRP", DayOfWeek.Wednesday, new DateTime(2000, 1, 1, 10, 30, 0), new TimeSpan(2, 0, 0), "John", "B-314"));
-            return testExport;
+            //Vasek: FIX ME 
+            //Misto bla bla prijde odkaz na singleton model, konkretne na jeho TimetableFields
+            //return ie.DownloadAsSVG(blabla);
+            return null;
         }
 
     }
