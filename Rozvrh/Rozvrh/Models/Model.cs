1404f1bb5812fd6a470b6548bead2067e144d97c
VHonzik <honzik.vaclav@gmail.com>
2013-11-21 19:54:33 +0100

    Removed two unused references to IExportHodina in model.

--- Rozvrh/Rozvrh/Models/Model.cs
+++ Rozvrh/Rozvrh/Models/Model.cs
@@ -22,15 +22,6 @@ namespace Rozvrh.Models
         /// </summary>
         public Model()
         {
-            // FIX ME !!! - autor: Richard
-            // Micha se export a model, pokud od sbe neco potrebuji, musi pres controller. 
-            // Pokud exporter potrebuje cokoli od modelu, controller si vse vytahne z modelu pred(!) zavooanim exporteru a rovnou mu posle vse potrebne.
-            // Proc je vubec v modelu IExportHodina? Pokud vidim dobre, pouziva tato trida jako nas datovy "typ" pro vypis vfiltrovanych hodin ve view.
-            // V tom pripade, je to nas zakladni datovy typ a mel by byt soucasti modelu a jmenovat se jinak, napr. FilteredLecture
-            // Nejsem si jisty, jestli je v tomto pripade nutny interface - pro zakladni datovy typ, ktery nema zadne funkce (vyjma get a set) - nebyla by vhodnejsi struktura?
-            // Navic mi prijde, ze ExportHodina se funkcne kryje s TimeTableField a slo by je sjednotit a zbavit se tak jedne datove tridy.
-            // Navrhuji refaktoring ve smyslu vyse uvedeneho, pokud ma nekdo jiny nazor - sem (resp. na FB nebo na SCRUM) s nim.
-            FiltredLectures = new List<IExportHodina>();
 
             System.Diagnostics.Debug.WriteLine("Model constructor");
             // Richard: This is only a temporary solution. In next phase the XmlLoader is gonna be modofied to be a singleton and these linq queries placed there.
@@ -233,10 +224,6 @@ namespace Rozvrh.Models
         //      V tuhle chvíli tedy v souboru HomeController, v metodě Filter.
         //      Jedná se totiž o aplikační logiku, nikoliv datovou.
 
-        /// <summary>
-        /// Seznam vyfiltrovaných hodin, určení pro zobrazení v tabulce pod filtry
-        /// </summary>
-        public List<IExportHodina>  FiltredLectures     { get; private set; }
 
         public List<Department>     Departments         { get; private set; }
         public List<Specialization> Specializations     { get; private set; }
