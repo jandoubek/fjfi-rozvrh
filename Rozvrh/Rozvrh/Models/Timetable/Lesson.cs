e79dd6a8095d9efca7ee36378e41f6f51009da2c
blatda <blatda@seznam.cz>
2013-11-26 19:38:55 +0100

    Lesson rollback

--- Rozvrh/Rozvrh/Models/Timetable/Lesson.cs
+++ Rozvrh/Rozvrh/Models/Timetable/Lesson.cs
@@ -7,13 +7,13 @@ namespace Rozvrh.Models.Timetable
     /// </summary>
     public class Lesson
     {
-        public string id            { get; private set; }
-        public string lectureId     { get; private set; }
-        public string lecturerId    { get; private set; }
-        public string dayId         { get; private set; }
-        public string timeId        { get; private set; }
-        public string classroomId   { get; private set; }
-        public string tag           { get; private set; }
+        public string id { get; private set; }
+        public string lectureId { get; private set; }
+        public string lecturerId { get; private set; }
+        public string dayId { get; private set; }
+        public string timeId { get; private set; }
+        public string classroomId { get; private set; }
+        public string tag { get; private set; }
 
         public Lesson(string id, string lectureId, string lecturerId, string dayId, string timeId, string classroomId, string tag)
         {
@@ -24,33 +24,6 @@ namespace Rozvrh.Models.Timetable
             this.timeId = timeId;
             this.classroomId = classroomId;
             this.tag = tag;
-        }
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hashCode = (id != null ? id.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (lectureId != null ? lectureId.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (lecturerId != null ? lecturerId.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (dayId != null ? dayId.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (timeId != null ? timeId.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (classroomId != null ? classroomId.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (tag != null ? tag.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((Lesson) obj);
-        }
-
-        protected bool Equals(Lesson other)
-        {
-            return string.Equals(id, other.id) && string.Equals(lectureId, other.lectureId) && string.Equals(lecturerId, other.lecturerId) && string.Equals(dayId, other.dayId) && string.Equals(timeId, other.timeId) && string.Equals(classroomId, other.classroomId) && string.Equals(tag, other.tag);
         }
     }
 }
