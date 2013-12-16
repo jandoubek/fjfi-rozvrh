817338069557b1c22bd1d113c53f83b585b3a664
blatda <blatda@seznam.cz>
2013-12-16 09:32:17 +0100

    onepage

--- Rozvrh/Rozvrh/App_Start/BundleConfig.cs
+++ Rozvrh/Rozvrh/App_Start/BundleConfig.cs
@@ -7,8 +7,6 @@ namespace Rozvrh
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/dateValidation").Include(
-                       "~/Scripts/dateValidation.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-2.0.3.js",
@@ -41,8 +39,7 @@ namespace Rozvrh
                         "~/Content/themes/base/jquery.ui.slider.css",
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.progressbar.css"));
         }
     }
 }
